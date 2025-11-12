c61fb59a33bd9a78873b71c87d5dec8da2a6afa7
SuharshaNunna <suha.nunna@gmail.com>
2025-11-12 17:26:30 -0500

    fixed king

--- KingPiece.cc
+++ KingPiece.cc
@@ -6,9 +6,12 @@ using namespace Student;
 
 bool KingPiece::canMoveToLocation(int toRow, int toColumn)
 {
+    if (toRow < 0 || toRow >= board.getNumRows() ||
+        toColumn < 0 || toColumn >= board.getNumCols())
+        return false;
+
     int dRow = std::abs(toRow - row);
     int dCol = std::abs(toColumn - col);
-
     if (dRow <= 1 && dCol <= 1 && (dRow + dCol > 0))
         return true;
 
