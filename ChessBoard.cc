eaa38e4ffb5eee0bfa78b30a1064db9dc41d5d2d
SuharshaNunna <suha.nunna@gmail.com>
2025-11-12 17:18:04 -0500

    adding king type

--- ChessBoard.cc
+++ ChessBoard.cc
@@ -34,6 +34,9 @@ void ChessBoard::createChessPiece(Color col, Type ty, int startRow, int startCol
     case Bishop:
         newPiece = new BishopPiece(*this, col, startRow, startColumn);
         break;
+    case King:                                       // ✅ ← ADD THIS
+        newPiece = new KingPiece(*this, col, startRow, startColumn);
+        break;
     }
 
     board.at(startRow).at(startColumn) = newPiece;
